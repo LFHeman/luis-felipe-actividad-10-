89027336dd04cc861da0b12df0d16a96122afbcd
luisfelipegs <luisfelipe14@live.com.mx>
2020-11-09 11:10:10 -0600

    funciones javascript

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// variables por tipo de datos
+const numerica = 10;
+const cadena = "hola a todos";
+const booleana = true;
+let sinValor;
+
+// Tipos de datos
+console.log(typeof numerica);
+console.log(typeof cadena);
+console.log(typeof booleana);
+console.log(typeof sinValor);
